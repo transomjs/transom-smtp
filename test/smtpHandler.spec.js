2ee7427d23eaf0956af22a549c7b903d316a6815
Mark Voorberg <mark@binaryops.ca>
2017-11-04 23:14:41 -0700

    formatting and code cleanup.

--- test/smtpHandler.spec.js
+++ test/smtpHandler.spec.js
@@ -5,7 +5,6 @@ const SmtpHandler = require('../lib/smtpHandler');
 const nodemailer = require('nodemailer');
 
 describe('SmtpHandler', function () {
-    // let testSmtp;
 
     beforeEach(function () {
         // Stub out a mock nodemailer
@@ -13,7 +12,6 @@ describe('SmtpHandler', function () {
             // whatever you would like innerLib.toCrazyCrap to do under test
             function Transport() {
                 this.sendMail = function (opts, cb) {
-                    console.log('Fake SendMail');
                     if (cb) {
                         return cb(opts.error, opts.response);
                     }
@@ -24,8 +22,12 @@ describe('SmtpHandler', function () {
         });
     });
 
+    afterEach(function () {
+        // restore original functionality
+        nodemailer.createTransport.restore();
+    });
+    
     it('can call createTransport', function () {
-
         const dummyServer = {};
         const dummyOptions = {};
         const transomSmtp = new SmtpHandler(dummyServer, dummyOptions);
@@ -49,7 +51,6 @@ describe('SmtpHandler', function () {
         sinon.assert.calledOnce(nodemailer.createTransport);
         expect(callback.calledOnce).to.be.true;
         expect(callback.calledWith('My-error'));
-
     });
 
     it('can callback with a response', function () {
@@ -64,11 +65,6 @@ describe('SmtpHandler', function () {
         sinon.assert.calledOnce(nodemailer.createTransport);
         expect(callback.calledOnce).to.be.true;
         expect(callback.calledWith(null, 'My-response'));
-
     });
 
-    afterEach(function () {
-        // restore original functionality
-        nodemailer.createTransport.restore();
-    });
 });
